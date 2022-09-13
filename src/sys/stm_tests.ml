b4c149370e11dcf04bf2a0121796da8fce440381
Lucccyo <cha.git@mailo.fr>
2022-09-13 10:58:37 +0200

    update file_exists

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -1,5 +1,100 @@
 open QCheck
 open STM
+
+module SConf = 
+struct
+  type cmd =
+    | File_exists of string list * string
+    [@@deriving show { with_path = false }]
+
+  type filesys = 
+    | Directory of {
+        perm: int;
+        dir_name: string;
+        mutable dir_list: filesys list
+      }
+    | File of {
+        perm: int;
+        file_name: string
+      }
+
+  type state = filesys
+  
+  type sut = unit
+
+  let arb_cmd _s =
+      let  str_gen = Gen.(oneofl ["aaa"]) in
+      let path_gen = Gen.(oneofl [["/root"]]) in
+      QCheck.make ~print:show_cmd 
+        Gen.(oneof
+              [ (map2 (fun path file_name -> File_exists (path, file_name))path_gen str_gen);
+              ])
+
+  let static_path = (Sys.getcwd ()) ^ "/sandbox"
+
+  let init_state  = Directory {perm = 0o777; dir_name = "/root"; dir_list = []}
+
+  let rec find (fs_list: filesys list) path name = match fs_list with  
+      | hd :: tl -> 
+        (match hd with
+          | Directory d -> Format.printf "size dir_list in %s : %d\n" d.dir_name (List.length d.dir_list);
+            Format.printf "%s--" d.dir_name;
+              if path = [] 
+                then (if d.dir_name = name 
+                        then (Format.printf "1- FOUND\n";Some hd)
+                        else (Format.printf "1- NO_SUCH_FILE\n"; None;))
+                else (if d.dir_name = List.hd path
+                        then (Format.printf "PERM\n"; find d.dir_list (List.tl path) name)
+                        else find tl path name)
+          | File f -> Format.printf "file : %s\n" f.file_name; if path = [] && f.file_name = name then (Format.printf "2- FOUND\n"; Some hd) else ( Format.printf "3- NO_SUCH_FILE\n"; None))
+      | []      ->  Format.printf "2- NO_SUCH_FILE\n"; None
+
+  let next_state c fs = match c with
+      | File_exists (path, file_name) -> 
+        Format.printf "path : ("; List.iter (Format.printf "%s ") path; Format.printf ")\t file_name : %s\n" file_name;
+        fs
+
+  let init_sut () = 
+      try 
+        Sys.mkdir (static_path ^ "/root") 0o777 
+      with Sys_error _ -> ()
+
+  let cleanup _   = ()
+
+  let precond c _s = match c with
+      | _ -> true
+
+  let run c _file_name = match c with
+      | File_exists (path_name, file_name) -> Format.printf "RUN : %s\n" (static_path  ^ (String.concat "/" path_name) ^ "/" ^ file_name);
+          Res (bool, Sys.file_exists (static_path ^ (String.concat "/" path_name) ^ "/" ^ file_name))
+
+  let filexists s path file_name = 
+      match (find [s] path file_name) with 
+        | Some _ -> true
+        | None   -> false
+
+  let postcond c (s:filesys) res = match c, res with
+      | File_exists (path_name, file_name), Res ((Bool,_),b) -> Format.printf "RUN RES : %b" b;
+        b = filexists s path_name file_name
+      | _,_ -> false
+
+end
+
+module SysSTM = STM.Make(SConf)
+
+;;
+Util.set_ci_printing ()
+;;
+QCheck_base_runner.run_tests_main
+  (let count = 1 in
+   [SysSTM.agree_test         ~count ~name:"STM Sys test sequential";
+])
+
+
+
+
+(* open QCheck
+open STM
 (* open Util *)
 
 (** parallel STM tests of Sys module*)
@@ -59,13 +154,25 @@ struct
             if path = [] 
               then (if d.dir_name = name 
                       then Some hd
-                      else None;)
+                      else (Format.printf "NO_SUCH_FILE\n"; None;))
               else (if d.dir_name = List.hd path
-                      then find d.dir_list (List.tl path) name
+                      then (Format.printf "PERM\n"; find d.dir_list (List.tl path) name)
                       else find tl path name)
-        | File f -> if path = [] && f.file_name = name then Some hd else None)
-    | []      ->  None
+        | File f -> if path = [] && f.file_name = name then Some hd else ( Format.printf "NO_SUCH_FILE\n"; None))
+    | []      ->  Format.printf "NO_SUCH_FILE\n"; None
 
+
+  let rec check_path (fs_list: filesys list) path = match fs_list with  
+    | hd :: tl -> 
+      (match hd with
+        | Directory d -> 
+            if path = [] 
+              then Some hd
+              else (if d.dir_name = List.hd path
+                      then (Format.printf "VERIF PERM\n" ; check_path d.dir_list (List.tl path))
+                      else check_path tl path)
+        | File _ -> if path = [] then Some hd else (Format.printf "NO_SUCH_FILE\n"; None))
+    | []      ->  Format.printf "NO_SUCH_FILE\n"; None
   (* let rec mkdir  (fs_list: filesys list) path dir_name perm = match fs_list with 
     | hd :: tl -> 
       (match hd with
@@ -80,22 +187,33 @@ struct
         | File _f -> ())
     | []      ->  () *)
 
-  let mkdir s path dir_name perm = 
-    match (find [s] path dir_name) with 
-    | Some fs -> (match fs with 
+  (* let rec mkdir fsl path dir_name perm = match fsl with *)
+      (* | hd :: tl -> (match hd with
+        | Directory d -> 
+          if d.dir_name = (List.hd path) 
+            then (VERIF PERM; )
+        | File f -> ) *)
+  let mkdir fs dir_name perm = match fs with
+    | Directory d -> d.dir_list <- List.cons (Directory {perm; dir_name; dir_list = []}) d.dir_list
+    | File _ -> Format.printf "ON VEUT UN DOSSIER\n"
+
+
+    (* | hd :: tl -> (match fs with 
       | Directory d ->  
         d.dir_list <- (List.cons (Directory {perm; dir_name; dir_list = []}) d.dir_list)
       | File _ -> ())
-    | None   -> ()
+    | None   -> () *)
 
   let next_state c fs = match c with
     | File_exists (_path, _file_name) -> fs
     (* | Remove      file_name -> List.filter (fun str -> str != file_name) l !! List.filter return a list and doesn't change the one in place *)
     | Mkdir (path, dir_name, perm) -> 
-      (* List.iter (Format.printf "%s ") path; Format.printf "\t dir_name = %s\n" dir_name; *)
-      (match find [fs] path dir_name with
-      | Some _ -> raise Division_by_zero
-      | None   -> Format.printf "[NS] on cree %s dans le model\n" dir_name; mkdir fs path dir_name perm; fs)
+      List.iter (Format.printf "%s ") path; Format.printf "\t dir_name = %s\n" dir_name;
+      (* let pre_dir  = List.hd (List.rev path) in
+      let pre_path = List.tl (List.rev path) in *)
+      (match check_path [fs] path with
+      | Some s -> Format.printf "[NS] Le path est correct\n"; mkdir s dir_name perm; fs
+      | None   -> Format.printf "[NS] Le path est incorrect et on a stocké les erreurs\n"; fs)
 
   (* let rec clear path = match Sys.is_directory path with
   | true ->
@@ -159,7 +277,7 @@ struct
     | File_exists (path_name, file_name), Res ((Bool,_),b) -> b = filexists s path_name file_name
     (* | Remove, Res ((Result (Unit,Exn),_),r) ->  r = Ok () *)
     | Mkdir (path, dir_name, _perm), Res ((Result (Unit,Exn),_),r) ->
-      Format.printf "[] %s\n" dir_name;
+      Format.printf "BOUH %s\n" dir_name;
       (match (find [s] path dir_name) with
         | Some _ -> r = Error (Sys_error (static_path ^ "/root" ^ "/" ^ dir_name ^ ": File exists"))
         | None   -> r = Ok ())
@@ -175,4 +293,4 @@ QCheck_base_runner.run_tests_main
   (let count = 1 in
    [SysSTM.agree_test         ~count ~name:"STM Sys test sequential";
     (* SysSTM.agree_test_par ~count ~name:"STM Sys test parallel"  *)
-])
+]) *)
