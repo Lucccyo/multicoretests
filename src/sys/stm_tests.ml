d18a249ed3274ada1e66a3e229f71899d5f7e9d1
Lucccyo <cha.git@mailo.fr>
2022-10-26 14:41:53 +0200

    fixing location sandbox_root

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -39,7 +39,7 @@ struct
                 map3 (fun path dir_name perm -> Mkdir (path, dir_name, perm)) path_gen str_gen perm_gen;
             ])
 
-  let static_path = Sys.getcwd () / "src/sys"
+  let static_path = Sys.getcwd ()
 
   let init_state  = 
     Directory {perm = 0o777; dir_name = "sandbox_root"; fs_list = []}
@@ -126,19 +126,19 @@ struct
     | Mkdir (path, dir_name, _perm), Res ((Result (Unit,Exn),_), Error (Sys_error (s) ))
       when s = (p path dir_name) ^ ": Permission denied"         -> 
         (* Format.printf "%s\n" (p path dir_name); *)
-        Format.printf "%s\n" (Sys.getcwd ());
+        (* Format.printf "%s\n" (Sys.getcwd ()); *)
         let b = not (is_perm_ok [fs] path) in
         b
     | Mkdir (path, dir_name, _perm), Res ((Result (Unit,Exn),_), Error (Sys_error (s) ))
       when s = (p path dir_name) ^ ": File exists"               -> 
-        Format.printf "%s\n" (Sys.getcwd ());
+        (* Format.printf "%s\n" (Sys.getcwd ()); *)
         (* Format.printf "%s\n" (p path dir_name); *)
         let b = mem fs path dir_name in
         b
     | Mkdir (path, dir_name, _perm), Res ((Result (Unit,Exn),_), Error (Sys_error (s) ))
       when s = (p path dir_name) ^ ": No such file or directory" ->
         (* Format.printf "%s\n" (p path dir_name);  *)
-        Format.printf "%s\n" (Sys.getcwd ());
+        (* Format.printf "%s\n" (Sys.getcwd ()); *)
         let b = (match path with
         | [] -> false
         | _hd_path :: _tl_path -> 
@@ -146,7 +146,7 @@ struct
           not (mem fs (List.rev (List.tl rev)) (List.hd rev))) in
         b
     | Mkdir (path, dir_name, _perm), Res ((Result (Unit,Exn),_), Ok ()) -> 
-      Format.printf "%s\n" (p path dir_name);
+      (* Format.printf "%s\n" (p path dir_name); *)
       let b = 
         match path with
         | [] -> true
